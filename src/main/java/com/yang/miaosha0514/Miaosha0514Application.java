f27ac27e1b4a0cd5002cda95a986bf1410d6277f
ytianjao <1064509110@qq.com>
2021-05-14 15:00:59 +0800

    初始化

---
+++ src/main/java/com/yang/miaosha0514/Miaosha0514Application.java
@@ -0,0 +1,13 @@
+package com.yang.miaosha0514;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miaosha0514Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Miaosha0514Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/yang/miaosha0514/Miaosha0514ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yang.miaosha0514;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miaosha0514ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
